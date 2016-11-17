d5b0ef74af65b19c5bccacb57f26951e3848965e
Amanda Steckler <amandajsteckler@gmail.com>
2016-11-17 18:11:18 -0500

    Done.

---
+++ components/Product.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.producer}</h2>
+        <p>{this.props.hasWatermark ? 'true' : 'false'}</p>
+        <p>{this.props.color}</p>
+        <p>{this.props.weight}</p>
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+};
+
+Product.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  producer: React.PropTypes.string,
+  hasWatermark: React.PropTypes.bool,
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, name) => {
+    const weight = props[name]
+    if (!(weight < 300 && weight > 80)) {
+      return new console.error('no')
+    }
+  }
+};
+
+module.exports = Product
